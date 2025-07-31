748c985625e8df3907a5f792f4f09d62334372c9
Slava <slavik3001@rambler.ru>
2025-07-31 21:33:22 +0300

    feat[GUI]: create tooltip window

---
+++ core/ui/tooltip.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+from core.config import TOOLTIP_X_PADDING, TOOLTIP_Y_PADDING, WHITE, MAIN_FONT
+
+
+class Tooltip:
+
+    def __init__(self, widget, text):
+        self.widget = widget
+        self.text = text
+        self.tooltip_window = None
+        widget.bind("<Enter>", self.show_tooltip)
+        widget.bind("<Leave>", self.hide_tooltip)
+
+    def show_tooltip(self, event):
+        if self.tooltip_window or not self.text:
+            return
+
+        x, y, _, _ = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + TOOLTIP_X_PADDING
+        y += self.widget.winfo_rooty() + TOOLTIP_Y_PADDING
+
+        self.tooltip_window = tw = tk.Toplevel(self.widget)
+        tw.wm_overrideredirect(True)
+        tw.geometry(f"+{x}+{y}")
+        label = tk.Label(tw, text=self.text, background=WHITE, relief="solid", borderwidth=1, font=MAIN_FONT)
+        label.pack(ipadx=1)
+
+    def hide_tooltip(self, event):
+        if self.tooltip_window:
+            self.tooltip_window.destroy()
+            self.tooltip_window = None
