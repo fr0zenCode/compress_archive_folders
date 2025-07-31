14556a6bcc9edd8a400833732baaeec153cf7cf0
Slava <slavik3001@rambler.ru>
2025-07-31 21:36:15 +0300

    refactor[GUI]: refactor

--- core/ui/main_window.py
+++ core/ui/main_window.py
@@ -1,109 +1,134 @@
+import ctypes
 import os
 import tkinter as tk
-from tkinter import filedialog, ttk
+from tkinter import filedialog
 
+import PIL.Image
 from PIL import ImageTk
 
 from .tooltip import Tooltip
-from ..archiver import zip_files, unzip_file
-from ..config import (
-    ENTRIES_WIDTH_PERCENTS,
-    SMALL_BUTTON_WIDTH,
-    ACTION_BUTTON_WIDTH,
-    PADDING_USUAL,
-    PADDING_FIRST_ELEMENT,
-    PADDING_LAST_ELEMENT,
-    MAIN_WINDOW_SIZE,
-    APP_NAME
+from .ui_widgets import (
+    setup_main_action_button_widget,
+    setup_main_label,
+    setup_browse_file_button,
+    setup_file_path_entry,
+    setup_yes_or_no_type_checkbox
 )
-from ..picture_utils import small_file_system_icon
+from ..archiver import zip_files, unzip_file
+from ..config import MAIN_WINDOW_SIZE, APP_NAME
+from ..picture_utils import small_file_system_icon, main_app_small_icon
 
 
 class ArchiveApp:
 
     def __init__(self, root):
+
         self.root = root
         self.root.title(APP_NAME)
         self.root.geometry(MAIN_WINDOW_SIZE)
         self.root.resizable(False, False)
+
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("Slava's Archive Machine Random String")
+
+        photo = PIL.ImageTk.PhotoImage(main_app_small_icon)
+        root.iconphoto(False, photo)
+
         self.setup_ui()
 
 
 
     def setup_ui(self):
-        padding_usual = PADDING_USUAL
-        padding_first_element = PADDING_FIRST_ELEMENT
-        padding_last_element = PADDING_LAST_ELEMENT
 
 
-        #---------------------------- СЕКЦИЯ КАРТИНОК ----------------------------#
+        #---------------------------- СОЗДАНИЕ ОБЪЕКТА ИЗОБРАЖЕНИЯ ----------------------------#
         self.small_file_system_icon_image = ImageTk.PhotoImage(small_file_system_icon)
 
 
         #---------------------------- СЕКЦИЯ АРХИВАЦИИ ----------------------------#
-        self.archivate_lbl = ttk.Label(self.root, text="Выберите директорию для архивирования*:")
-        self.archivate_lbl.grid(row=0, column=0, sticky="w", **padding_first_element)
+        self.archivate_lbl = setup_main_label(
+            root=self.root,
+            text="Выберите директорию для архивирования*:",
+            row=0,
+            column=0,
+            sticky="w"
+        )
         Tooltip(
             self.archivate_lbl,
-            "Все папки, находящиеся в выбраннной папке, не имеющие расширение .zip будут заархивированы."
+            "Все папки, находящиеся в выбранной папке, не имеющие расширение .zip, будут заархивированы."
         )
 
-        self.archive_dir_entry = ttk.Entry(self.root, width=ENTRIES_WIDTH_PERCENTS)
-        self.archive_dir_entry.grid(row=1, column=0, sticky="w", **padding_usual)
+        self.archive_dir_entry = setup_file_path_entry(root=self.root, row=1, column=0, sticky="w")
 
-        self.delete_origin_file_after_zip = tk.BooleanVar()
-        self.c1 = ttk.Checkbutton(self.root, text="Удалить исходный файл после архивации", onvalue=True, offvalue=False, variable=self.delete_origin_file_after_zip)
-        self.c1.grid(row=2, column=0, sticky="w", **padding_usual)
-
-        self.browse_dir_button = ttk.Button(
-            self.root,
-            width=SMALL_BUTTON_WIDTH,
-            image=self.small_file_system_icon_image,
-            command=self.browse_directory
+        self.delete_origin_file_after_zip_var = tk.BooleanVar()
+        self.delete_origin_file_after_zip_checkbox = setup_yes_or_no_type_checkbox(
+            root=self.root,
+            text="Удалить исходный файл после архивации",
+            onvalue=True,
+            offvalue=False,
+            variable=self.delete_origin_file_after_zip_var,
+            row=2,
+            column=0,
+            sticky="w"
         )
-        self.browse_dir_button.grid(row=1, column=1, sticky="w")
+        self.browse_dir_for_archive_button = setup_browse_file_button(
+            root=self.root,
+            command=self.browse_directory,
+            row=1,
+            column=1,
+            sticky="w",
+            image=self.small_file_system_icon_image
+        )
 
-        archive_button = ttk.Button(
-            self.root,
+        self.archive_button = setup_main_action_button_widget(
+            root=self.root,
             text="Архивировать",
-            width=ACTION_BUTTON_WIDTH,
-            command=(lambda: self.archive_selected_folder(self.delete_origin_file_after_zip))
+            command=lambda: self.archive_selected_folder(bool(self.delete_origin_file_after_zip_var)),
+            row=3,
+            column=0,
+            sticky="w"
         )
-        archive_button.grid(row=3, column=0, sticky="w", **padding_last_element)
 
 
         #---------------------------- СЕКЦИЯ ДЕ-АРХИВАЦИИ ----------------------------#
-        tk.Label(self.root, text="Выберите файл для де-архивации*:").grid(
+        self.unarchive_label = setup_main_label(
+            root=self.root,
+            text="Выберите файл для де-архивации*:",
             row=4,
             column=0,
+            sticky="w"
+        )
+        Tooltip(self.unarchive_label, "Выбранный файл будет разархивирован.")
+
+        self.unzip_file_entry = setup_file_path_entry(root=self.root, row=5, column=0, sticky="w")
+        self.browse_file_for_unarchive_button = setup_browse_file_button(
+            root=self.root,
+            command=self.browse_zip_file,
+            row=5,
+            column=1,
             sticky="w",
-            **padding_usual
+            image=self.small_file_system_icon_image
         )
 
-        self.unzip_file_entry = ttk.Entry(self.root, width=ENTRIES_WIDTH_PERCENTS)
-        self.unzip_file_entry.grid(row=5, column=0, sticky="w", **padding_usual)
-
-        self.browse_file_button = ttk.Button(
-            self.root,
-            width=SMALL_BUTTON_WIDTH,
-            image=self.small_file_system_icon_image,
-            command=self.browse_zip_file
+        self.delete_origin_file_after_unzip_var = tk.BooleanVar()
+        self.delete_origin_file_after_unzip_checkbox = setup_yes_or_no_type_checkbox(
+            root=self.root,
+            text="Удалить исходный файл после разархивации",
+            onvalue=True,
+            offvalue=False,
+            variable=self.delete_origin_file_after_unzip_var,
+            row=6,
+            column=0,
+            sticky="w"
         )
-        self.browse_file_button.grid(row=5, column=1, sticky="w")
 
-        self.delete_origin_file_after_unzip = tk.BooleanVar()
-        self.c1 = ttk.Checkbutton(self.root, text="Удалить исходный файл после де-архивации", onvalue=True, offvalue=False,
-                                  variable=self.delete_origin_file_after_unzip)
-        self.c1.grid(row=6, column=0, sticky="w", **padding_usual)
-
-
-        unzip_button = ttk.Button(
-            self.root,
+        self.unarchive_button = setup_main_action_button_widget(
+            root=self.root,
             text="Разархивировать",
-            width=ACTION_BUTTON_WIDTH,
-            command=(lambda: self.unzip_selected_file(self.delete_origin_file_after_unzip))
+            command=lambda: self.unzip_selected_file(bool(self.delete_origin_file_after_unzip_var)),
+            row=7,
+            column=0,
+            sticky="w"
         )
-        unzip_button.grid(row=7, column=0, sticky="w", **padding_usual)
 
 
     def browse_directory(self):
@@ -126,4 +151,8 @@ class ArchiveApp:
     def unzip_selected_file(self, delete_origin_zip_file: bool):
         zip_path = self.unzip_file_entry.get()
         if zip_path:
-            unzip_file(zip_path, save_unzip_file_path=os.path.dirname(zip_path), delete_origin_zip_file=delete_origin_zip_file)
+            unzip_file(
+                zip_path,
+                save_unzip_file_path=os.path.dirname(zip_path),
+                delete_origin_zip_file=delete_origin_zip_file
+            )
